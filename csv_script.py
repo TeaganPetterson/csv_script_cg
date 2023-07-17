020f72fe72b108dd88c1c07cf2d70e29de9195e0
Teagan Petterson <teagan.petterson1@gmail.com>
2023-07-17 16:27:49 -0600

    got the script making a new file with the correct info

--- csv_script.py
+++ csv_script.py
@@ -1,6 +1,22 @@
-from csv_diff import load_csv, compare
-diff = compare(
-    load_csv(open("enterprise.csv"), key="Email"),
-    load_csv(open("rc.csv"), key="Email")
-)
-print(diff)
+import csv
+
+with open("enterprise.csv") as enterpriseFile, open("rc.csv") as rcFile, open("updated_rc.csv", "w", newline='') as updatedFile:
+    enterpriseData = list(csv.reader(enterpriseFile, delimiter=','))
+    rcData = list(csv.reader(rcFile, delimiter=','))
+    first_ent_row = enterpriseData[0]
+    first_rc_row = rcData[0]
+    ent_phone_spot = first_ent_row.index("Mobile Phone")
+    rc_phone_spot = first_rc_row.index("Mobile Phone")
+
+    for rcRow in rcData[1:]:
+        if rcRow[rc_phone_spot] == '':
+            for entRow in enterpriseData[1:]:
+                if rcRow[rc_phone_spot - 1] == entRow[ent_phone_spot - 1]:
+                    rcRow[rc_phone_spot] = entRow[ent_phone_spot]
+                    break
+
+    writer = csv.writer(updatedFile)
+    writer.writerow(first_rc_row) 
+    writer.writerows(rcData[1:]) 
+
+print("Script execution completed.")
