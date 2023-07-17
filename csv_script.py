90aa7b02dbdd0705de980d0f881be425a3d4392a
Teagan Petterson <teagan.petterson1@gmail.com>
2023-07-17 15:41:29 -0600

    comparing differences

--- csv_script.py
+++ csv_script.py
@@ -1,5 +1,6 @@
 from csv_diff import load_csv, compare
 diff = compare(
-    load_csv(open("one.csv"), key="id"),
-    load_csv(open("two.csv"), key="id")
+    load_csv(open("enterprise.csv"), key="Email"),
+    load_csv(open("rc.csv"), key="Email")
 )
+print(diff)
